d49a2695d1646e26e8141a33bde74d43bbdc963f
HenriqueOkuti <henrique.krastins@gmail.com>
2022-08-02 09:08:11 -0300

    Imported fonts: Playball & Lexend Deca

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,6 +1,21 @@
+import styled from "styled-components";
+import FontStyles from "../shared/Vars";
+
+
 export default function App() {
     return (
         <>
+            <FontStyles />
+            <Teste1 className="teste1">TrackIt</Teste1>
+            <Teste2 className="teste2">Texto</Teste2>
         </>
     );
 }
+
+const Teste1 = styled.div`
+    font-family: 'Playball';
+`
+
+const Teste2 = styled.div`
+    font-family: 'Lexend Deca';
+`
---
+++ src/shared/Vars.js
@@ -0,0 +1,24 @@
+import Playball from './Playball/Playball-Regular.ttf';
+//import LexendDeca from './Lexend_Deca/static/LexendDeca-Regular.ttf';
+import LexendDeca from './Lexend_Deca/LexendDeca-VariableFont_wght.ttf';
+import { createGlobalStyle } from 'styled-components';
+
+const FontStyles = createGlobalStyle`
+    @font-face {
+        font-family: 'Playball';
+        src: url(${Playball}) format('truetype');
+        font-weight: 400;
+        font-style: normal;
+        font-display: auto;
+    }
+
+    @font-face {
+        font-family: 'Lexend Deca';
+        src: url(${LexendDeca}) format('truetype');
+        font-weight: 400;
+        font-style: normal;
+        font-display: auto;
+    }   
+`;
+
+export default FontStyles;
